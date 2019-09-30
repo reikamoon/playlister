dbad9daeccf8f57f77f82bade5bb9e0bf8aeb8a9
Anjelica <ambrosio.anjelica@gmail.com>
2019-09-30 13:13:04 -0700

    added new route

--- app.py
+++ app.py
@@ -28,5 +28,11 @@ def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_new.html')
 
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    print(request.form.to_dict())
+    return redirect(url_for('playlists_index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
