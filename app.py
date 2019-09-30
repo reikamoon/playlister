963600e3588f729469d41a44f7a7ba032cb7b4ad
Anjelica <ambrosio.anjelica@gmail.com>
2019-09-30 13:09:54 -0700

    added new playlist route

--- app.py
+++ app.py
@@ -22,7 +22,11 @@ def index():
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
-
+    
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
