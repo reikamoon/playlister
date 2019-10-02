84b4c371350dd351f455f07c635b0921068fee19
Anjelica <ambrosio.anjelica@gmail.com>
2019-10-01 18:41:32 -0700

    changed client

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 comments = db.comments
